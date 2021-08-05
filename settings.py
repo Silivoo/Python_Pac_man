3b4e0d9c5cdc97b51f4d64df4a2888d2bad59b97
TheyCallMePie <aidog100doesgaming@gmail.com>
2021-08-05 19:01:55 -0400

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,25 @@
+from pygame.math import Vector2 as vec
+
+
+# Screen Settings
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
+
+# colour settings
+BLACK = (0,0,0)
+RED = (208, 22, 22)
+WHITE = (255, 255, 255)
+GREY = (107, 107, 107)
+BLUE = (59, 169, 196)
+
+# Font Settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+# player settigns
+#PLAYER_START_POS = 0
+PLAYER_COLOR = (190, 194, 15)
+
+# mob settings
